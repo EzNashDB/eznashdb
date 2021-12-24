b7eacb42ee754635a3bb9ec0022b6610b9a52a90
Ari Abramowitz <ari.abramowitz1@gmail.com>
2021-12-24 13:19:09 +0200

    Create local dev db

--- app/settings.py
+++ app/settings.py
@@ -12,7 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from os.path import dirname, join
 import dj_database_url
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), "../.env")
+load_dotenv(dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
