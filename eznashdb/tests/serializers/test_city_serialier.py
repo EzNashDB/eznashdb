db456f8b6e20fe64ad0cff5a2a4a6f616618ed4d
Ari Abramowitz <ari.abramowitz1@gmail.com>
2022-02-02 22:21:59 +0200

    Cleanup tests

--- eznashdb/tests/serializers/test_city_serialier.py
+++ eznashdb/tests/serializers/test_city_serialier.py
@@ -2,13 +2,16 @@ from eznashdb.models import Country, Region, City
 from eznashdb.serializers import CitySerializer
 
 def test_converts_city_to_dict():
+    # Arrange
     country = Country.objects.create(name='United States', short_name="US")
     region = Region.objects.create(country=country, name="New Jersey", is_default_region=False)
     city = City.objects.create(region=region, name="Teaneck", latitude="1", longitude="2")
 
+    # Act
     serializer = CitySerializer(city, context={'request': None})
     data = serializer.data
 
+    # Assert
     assert data["id"] == city.id
     assert data["name"] == city.name
     assert data["latitude"] == city.latitude
