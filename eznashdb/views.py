a108caf8a29eb6b1830377c81cd1e9c19d987a03
Ari Abramowitz <ari.abramowitz1@gmail.com>
2022-02-14 22:13:26 +0200

    Add delete shul endpoint

--- eznashdb/views.py
+++ eznashdb/views.py
@@ -1,14 +1,21 @@
 from rest_framework import generics
 from eznashdb.models import Shul
 from eznashdb.serializers import ShulSerializer
+from rest_framework import permissions
 
 
-class ShulDetail(generics.RetrieveAPIView):
+class ShulDetail(generics.RetrieveDestroyAPIView):
     """
     Retrieve a shul
     """
     queryset = Shul.objects.all()
     serializer_class = ShulSerializer
+
+    def get_permissions(self):
+        if self.request.method in ['DELETE']:
+            return [permissions.IsAuthenticated()]
+        return []
+
 
 class ShulList(generics.ListAPIView):
     """
