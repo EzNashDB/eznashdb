115a54c3de446ea9e7fef96a7ad8aa317d169fac
Ari Abramowitz <ari.abramowitz1@gmail.com>
2022-12-03 22:33:07 +0200

    Add shuls table on home page

---
+++ eznashdb/templatetags/eznashdb_tags.py
@@ -0,0 +1,17 @@
+from django import template
+from django.utils.safestring import mark_safe
+register = template.Library()
+
+@register.filter
+def bool_to_icon(value: bool) -> str:
+    icons = {
+        True: "fa-check",
+        False: "fa-times",
+    }
+    try:
+        icon = icons[value]
+    except KeyError:
+        return value
+    return mark_safe(
+        f"<i class=\"fa {icon}\" aria-hidden=\"true\"></i>"
+    )
--- eznashdb/views.py
+++ eznashdb/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
-
+from eznashdb.models import Shul
 
 class HomeView(TemplateView):
     template_name = "eznashdb/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["shuls"] = Shul.objects.all()
+        return context
+    
