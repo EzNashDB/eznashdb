ec7843830379a7aa83fcb7240db80ec1c2a795c4
Ari Abramowitz <ari.abramowitz1@gmail.com>
2022-12-04 19:57:14 +0200

    Highlight current navabr link

--- conftest.py
+++ conftest.py
@@ -1,4 +1,7 @@
 import pytest
+from django.urls import resolve, reverse
+from django.core.handlers.wsgi import WSGIRequest
+from typing import Callable
 
 @pytest.fixture(autouse=True)
 def enable_db_access_for_all_tests(db):
@@ -11,3 +14,12 @@ def test_user(django_user_model):
     )
     user.set_password("password")
     return user
+
+@pytest.fixture()
+def GET_request_factory(rf) -> Callable:
+    def _GET_request(view_name: str) -> WSGIRequest:
+        url = reverse(view_name)
+        request = (rf.get(url))
+        request.resolver_match = resolve(url)
+        return request
+    return _GET_request
--- eznashdb/tests/views/test_home_view.py
+++ eznashdb/tests/views/test_home_view.py
@@ -1,33 +1,31 @@
 from eznashdb.views import HomeView
 from bs4 import BeautifulSoup
 from eznashdb.models import Shul
-def test_shows_app_name(rf):
-    request = (
-        rf.get("/")
-    )
-    response = HomeView.as_view()(request)
+import pytest
+
+@pytest.fixture()
+def GET_request(GET_request_factory):
+    return GET_request_factory('eznashdb:home')
+
+def test_shows_app_name(GET_request):
+    response = HomeView.as_view()(GET_request)
     soup = BeautifulSoup(str(response.render().content), features="html.parser")
 
     assert "Ezrat Nashim Database" in soup.get_text()
 
-def test_lists_shuls(rf, test_user):
+def test_lists_shuls(GET_request, test_user):
     shul = Shul.objects.create(
         created_by=test_user,
         name="test shul"
     )
-    request = (
-        rf.get("/")
-    )
-    response = HomeView.as_view()(request)
+
+    response = HomeView.as_view()(GET_request)
     soup = BeautifulSoup(str(response.render().content), features="html.parser")
 
     assert shul.name in soup.get_text()
 
-def test_shows_message_if_no_shuls(rf):
-    request = (
-        rf.get("/")
-    )
-    response = HomeView.as_view()(request)
+def test_shows_message_if_no_shuls(GET_request):
+    response = HomeView.as_view()(GET_request)
     soup = BeautifulSoup(str(response.render().content), features="html.parser")
 
     assert "No Shuls Found" in soup.get_text()
