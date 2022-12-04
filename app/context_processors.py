f19ef71da1044250f1778b294b9d11ceda0ef979
Ari Abramowitz <ari.abramowitz1@gmail.com>
2022-12-04 19:57:14 +0200

    Highlight current navabr link

---
+++ app/context_processors.py
@@ -0,0 +1,13 @@
+from collections import namedtuple
+
+def navbar(request):
+    NavbarItem = namedtuple("NavbarItem", "label view_name")
+    current_app_name = request.resolver_match.app_names[0]
+    current_url_name = request.resolver_match.url_name
+    return {
+        'current_view_name': f"{current_app_name}:{current_url_name}",
+        'navbar_items': [
+            NavbarItem('Home', 'eznashdb:home'),
+            NavbarItem('Add a Shul', 'eznashdb:create_shul')
+        ]
+    }
--- eznashdb/tests/views/test_create_update_shul_view.py
+++ eznashdb/tests/views/test_create_update_shul_view.py
@@ -1,13 +1,13 @@
 from eznashdb.views import CreateUpdateShulView
 from bs4 import BeautifulSoup
-from eznashdb.models import Shul
+import pytest
 
-def test_shows_page_title(rf):
-    request = (
-        rf.get("/")
-    )
-    response = CreateUpdateShulView.as_view()(request)
+@pytest.fixture()
+def GET_request(GET_request_factory):
+    return GET_request_factory('eznashdb:create_shul')
+
+def test_shows_page_title(GET_request):
+    response = CreateUpdateShulView.as_view()(GET_request)
     soup = BeautifulSoup(str(response.render().content), features="html.parser")
 
     assert "add a shul" in soup.get_text().lower()
-
