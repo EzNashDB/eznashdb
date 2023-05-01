9e20769ee44f1d893657d0b2dab05e531ac321c2
Ari Abramowitz <ari.abramowitz1@gmail.com>
2023-05-01 21:01:25 +0300

    feat: Add rooms in seed_db

--- app/management/commands/seed_db.py
+++ app/management/commands/seed_db.py
@@ -8,17 +8,34 @@ User = get_user_model()
 
 class Command(BaseCommand):
     help = "Seed the database"
+    user: User = None
+    shul_count = 3
+    rooms_per_shul = 2
 
     def handle(self, *args: Any, **options: Any) -> Optional[str]:
-        user, _ = User.objects.get_or_create(username="db_seeder")
-        user.set_password("password")
-        for i in range(3):
-            Shul.objects.update_or_create(
+        self.user, _ = User.objects.get_or_create(username="db_seeder")
+        self.user.set_password("password")
+        self._create_shuls()
+
+    def _create_shuls(self):
+        for i in range(self.shul_count):
+            shul, _ = Shul.objects.update_or_create(
                 name=f"Seeded Shul {i+1}",
                 defaults={
-                    "created_by": user,
+                    "created_by": self.user,
                     "has_female_leadership": bool(i % 2),
                     "has_childcare": bool((i + 1) % 2),
                     "can_say_kaddish": bool(i % 2),
+                },
+            )
+            self._create_shul_rooms(shul)
+
+    def _create_shul_rooms(self, shul):
+        shul_num = shul.name[-1]
+        for j in range(self.rooms_per_shul):
+            shul.rooms.update_or_create(
+                name=f"Seeded Room {shul_num}-{j+1}",
+                defaults={
+                    "created_by": self.user,
                 },
             )
--- app/tests/management/commands/test_seed_db.py
+++ app/tests/management/commands/test_seed_db.py
@@ -1,20 +1,37 @@
 from django.core.management import call_command
 from eznashdb.models import Shul
 
+
 def test_creates_a_user(django_user_model):
     call_command("seed_db")
     assert django_user_model.objects.count() == 1
+
 
 def test_does_not_recreate_user(django_user_model):
     call_command("seed_db")
     call_command("seed_db")
     assert django_user_model.objects.count() == 1
 
+
 def test_creates_3_shuls():
     call_command("seed_db")
     assert Shul.objects.count() == 3
+
 
 def test_does_not_recreate_shuls():
     call_command("seed_db")
     call_command("seed_db")
     assert Shul.objects.count() == 3
+
+
+def test_creates_2_rooms_per_shul():
+    call_command("seed_db")
+    for shul in Shul.objects.all():
+        assert shul.rooms.count() == 2
+
+
+def test_does_not_recreate_rooms():
+    call_command("seed_db")
+    call_command("seed_db")
+    for shul in Shul.objects.all():
+        assert shul.rooms.count() == 2
