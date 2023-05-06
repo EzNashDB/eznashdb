5d1476a898f8335617777c93b1642136d0c563c5
Ari Abramowitz <ari.abramowitz1@gmail.com>
2023-05-06 23:16:36 +0300

    feat: Add initial room layout data on list page

--- eznashdb/tests/templatetags/eznashdb_tags/test_bool_to_icon.py
+++ eznashdb/tests/templatetags/eznashdb_tags/test_bool_to_icon.py
@@ -1,16 +1,17 @@
 from eznashdb.templatetags.eznashdb_tags import bool_to_icon
 import pytest
 
+
 @pytest.mark.parametrize(
-    "value,icon",
+    "value,icon_class",
     [
         (True, "fa-check"),
         (False, "fa-times"),
     ],
 )
-def test_converts_bool_to_font_awesome_icon(value, icon):
-    icon_html = f"<i class=\"fa {icon}\" aria-hidden=\"true\"></i>"
-    assert bool_to_icon(value) == icon_html
+def test_converts_bool_to_font_awesome_icon(value, icon_class):
+    assert icon_class in bool_to_icon(value)
+
 
 def test_passes_invalid_value_through():
     assert bool_to_icon("invalid") == "invalid"
