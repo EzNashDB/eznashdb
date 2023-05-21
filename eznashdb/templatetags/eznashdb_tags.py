921b6139add09e75a5252151b5b6b3cf83149509
Ari Abramowitz <ari.abramowitz1@gmail.com>
2023-05-21 11:14:15 +0300

    feat: Merge balcony fields (is_back, is_side -> is_balcony)

--- eznashdb/templatetags/eznashdb_tags.py
+++ eznashdb/templatetags/eznashdb_tags.py
@@ -42,19 +42,18 @@ def score_to_stars(value) -> str:
 
 
 ROOM_LAYOUT_DISPLAY_VALUES_BY_TYPE = {
-    "Balcony": {
-        "is_balcony_side": "Side",
-        "is_balcony_back": "Back",
-    },
     "Same floor": {
         "is_same_floor_side": "Side",
         "is_same_floor_back": "Back",
         "is_same_floor_elevated": "Elevated",
         "is_same_floor_level": "Level",
     },
+    "Balcony": {
+        "is_balcony": "",
+    },
     "No women's section": {
-        "is_only_men": "Only Men",
-        "is_mixed_seating": "Mixed Seating",
+        "is_only_men": "Only men",
+        "is_mixed_seating": "Mixed seating",
     },
 }
 
@@ -74,6 +73,8 @@ class RoomLayoutTypeDisplayData:
     @property
     def as_string(self) -> str:
         display_values_dict = ROOM_LAYOUT_DISPLAY_VALUES_BY_TYPE[self.layout_type]
+        if len(display_values_dict.values()) == 1:
+            return
         display_values = [
             v for k, v in display_values_dict.items() if getattr(self.room, k)
         ]
