a63f39c149abeb3799408e42577ca45962f3d254
Ari Abramowitz <ari.abramowitz1@gmail.com>
2023-05-23 22:27:39 +0300

    feat: Add female leadership filter

--- eznashdb/filtersets.py
+++ eznashdb/filtersets.py
@@ -1,11 +1,31 @@
-from django_filters import CharFilter, FilterSet
+from django.db.models import Q
+from django_filters import CharFilter, FilterSet, MultipleChoiceFilter
 
 from eznashdb.models import Shul
+from eznashdb.widgets import TomSelectWidget
 
 
 class ShulFilterSet(FilterSet):
-    name = CharFilter(lookup_expr="icontains", label="Name")
+    name = CharFilter(lookup_expr="icontains", label="Shul Name")
+    has_female_leadership = MultipleChoiceFilter(
+        choices=(
+            ("--", "Unknown"),
+            (True, "Yes"),
+            (False, "No"),
+        ),
+        widget=TomSelectWidget(),
+        label="Female Leadership",
+        method="filter_has_female_leadership",
+    )
+
+    def filter_has_female_leadership(self, qs, name, value):
+        include_None = "--" in value
+        values = [v for v in value if v != "--"]
+        query = Q(has_female_leadership__in=values)
+        if include_None:
+            query |= Q(has_female_leadership__isnull=True)
+        return qs.filter(query)
 
     class Meta:
         model = Shul
-        fields = ["name"]
+        fields = ["name", "has_female_leadership"]
--- eznashdb/tests/filtersets/test_shul_filterset.py
+++ eznashdb/tests/filtersets/test_shul_filterset.py
@@ -13,7 +13,7 @@ def test_request(rf, test_user):
     return request
 
 
-def describe_name_filter():
+def describe_shul_name():
     @pytest.mark.parametrize(
         ("name", "query"),
         [
@@ -31,3 +31,44 @@ def describe_name_filter():
         Shul.objects.create(created_by=test_user, name="test shul")
 
         assert ShulFilterSet({"name": "no match"}, request=test_request).qs.count() == 0
+
+
+def describe_has_female_leadership():
+    @pytest.mark.parametrize(
+        ("value", "query"),
+        [
+            (True, "True"),
+            (False, "False"),
+            (None, "--"),
+        ],
+    )
+    def includes_shuls_that_match_single_value(test_user, test_request, value, query):
+        Shul.objects.create(created_by=test_user, has_female_leadership=value)
+
+        assert ShulFilterSet({"has_female_leadership": [query]}, request=test_request).qs.count() == 1
+
+    @pytest.mark.parametrize(
+        ("value", "query"),
+        [
+            (True, ["True", "False"]),
+            (False, ["False", "--"]),
+            (None, ["True", "--"]),
+        ],
+    )
+    def includes_shuls_that_match_any_of_multiple_values(test_user, test_request, value, query):
+        Shul.objects.create(created_by=test_user, has_female_leadership=value)
+
+        assert ShulFilterSet({"has_female_leadership": [query]}, request=test_request).qs.count() == 1
+
+    @pytest.mark.parametrize(
+        ("value", "query"),
+        [
+            (True, ["False", "--"]),
+            (False, ["True", "--"]),
+            (None, ["True", "False"]),
+        ],
+    )
+    def excludes_shuls_that_do_not_match_any_of_value(test_user, test_request, value, query):
+        Shul.objects.create(created_by=test_user, has_female_leadership=value)
+
+        assert ShulFilterSet({"has_female_leadership": [query]}, request=test_request).qs.count() == 1
