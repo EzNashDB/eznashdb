d395758c68e435c1bfa81235cd9e864ac1bb6abf
Ari Abramowitz <ari.abramowitz1@gmail.com>
2023-05-24 11:16:10 +0300

    feat: Add test for has_female_leadership filter

--- eznashdb/tests/views/test_shuls_filter_view.py
+++ eznashdb/tests/views/test_shuls_filter_view.py
@@ -173,3 +173,15 @@ def describe_filter():
         soup_text = soup.get_text().lower()
         assert "shul 2" in soup_text
         assert "shul 1" not in soup_text
+
+    def filters_by_has_female_leadership(rf_GET, test_user):
+        Shul.objects.create(created_by=test_user, name="shul 1", has_female_leadership=False)
+        Shul.objects.create(created_by=test_user, name="shul 2", has_female_leadership=True)
+        request = rf_GET("eznashdb:shuls", {"has_female_leadership": ["True"]})
+
+        response = ShulsFilterView.as_view()(request)
+
+        soup = BeautifulSoup(str(response.render().content), features="html.parser")
+        soup_text = soup.get_text().lower()
+        assert "shul 2" in soup_text
+        assert "shul 1" not in soup_text
