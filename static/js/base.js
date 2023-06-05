1f0dcda87c02355252be942e23f9ec291160de36
Ari Abramowitz <ari.abramowitz1@gmail.com>
2023-06-05 23:31:44 +0300

    feat: Add tooltip initializaer in base.js

--- static/js/base.js
+++ static/js/base.js
@@ -6,6 +6,11 @@
     resetBodyHeight();
 })();
 
+(function initializeTooltips(){
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+})();
+
 $(document).ready(function() {
     $('.bs-multiselect').multiselect({
         includeSelectAllOption: true,
