0774e3a8848896bedc255e7046cebc111fe143cb
Ari Abramowitz <ari.abramowitz1@gmail.com>
2023-06-07 20:57:04 -0400

    feat: Add prettierJS precommit check

--- static/js/base.js
+++ static/js/base.js
@@ -1,44 +1,45 @@
-(function setBodyHeight(){
-    function resetBodyHeight(){
-        document.body.style.height = window.innerHeight + "px";
-    }
-    window.addEventListener("resize", resetBodyHeight);
-    resetBodyHeight();
+(function setBodyHeight() {
+  function resetBodyHeight() {
+    document.body.style.height = window.innerHeight + "px";
+  }
+  window.addEventListener("resize", resetBodyHeight);
+  resetBodyHeight();
 })();
 
-(function initializeTooltips(){
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+(function initializeTooltips() {
+  const tooltipTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]'
+  );
+  const tooltipList = [...tooltipTriggerList].map(
+    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+  );
 })();
 
-$(document).ready(function() {
-    $('.bs-multiselect').multiselect({
-        includeSelectAllOption: true,
-        buttonClass: 'form-select',
-        buttonWidth: '100%',
-        buttonText: function(options, select) {
-            if (options.length === 0) {
-                return '--------';
-            }
-            else if (options.length > 1) {
-                return `${options.length} selected`;
-            }
-            else {
-                var labels = [];
-                options.each(function() {
-                    if ($(this).attr('label') !== undefined) {
-                        labels.push($(this).attr('label'));
-                    }
-                    else {
-                        labels.push($(this).html());
-                    }
-                });
-                return labels.join(', ') + '';
-            }
-        },
-        templates: {
-            button: '<button type="button" class="multiselect dropdown-toggle d-block" data-bs-toggle="dropdown"><div class="multiselect-selected-text text-start"></div></button>',
-
-        }
-    });
+$(document).ready(function () {
+  $(".bs-multiselect").multiselect({
+    includeSelectAllOption: true,
+    buttonClass: "form-select",
+    buttonWidth: "100%",
+    buttonText: function (options, select) {
+      if (options.length === 0) {
+        return "--------";
+      } else if (options.length > 1) {
+        return `${options.length} selected`;
+      } else {
+        var labels = [];
+        options.each(function () {
+          if ($(this).attr("label") !== undefined) {
+            labels.push($(this).attr("label"));
+          } else {
+            labels.push($(this).html());
+          }
+        });
+        return labels.join(", ") + "";
+      }
+    },
+    templates: {
+      button:
+        '<button type="button" class="multiselect dropdown-toggle d-block" data-bs-toggle="dropdown"><div class="multiselect-selected-text text-start"></div></button>',
+    },
+  });
 });
