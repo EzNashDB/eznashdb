e4e44136d73b37f4ca26900ccfddef41428f1597
Ari Abramowitz <ari.abramowitz1@gmail.com>
2023-06-26 21:21:20 +0300

    feat: Add initial room formset

--- eznashdb/tests/forms/test_create_shul_form.py
+++ eznashdb/tests/forms/test_create_shul_form.py
@@ -10,13 +10,3 @@ def test_displays_shul_fields():
     assert soup.find(attrs={"id": "id_has_female_leadership"})
     assert soup.find(attrs={"id": "id_has_childcare"})
     assert soup.find(attrs={"id": "id_can_say_kaddish"})
-
-
-def test_displays_room_fields():
-    form = CreateShulForm()
-    soup = BeautifulSoup(form.as_p(), "html.parser")
-    assert soup.find(attrs={"id": "id_room_name"})
-    assert soup.find(attrs={"id": "id_room_layout"})
-    assert soup.find(attrs={"id": "id_room_wheelchair_access"})
-    assert soup.find(attrs={"id": "id_room_see_hear"})
-    assert soup.find(attrs={"id": "id_room_relative_size"})
