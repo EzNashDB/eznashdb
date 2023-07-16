621134e48e9fd09d4ae79917581db46d28d2ed18
Ari Abramowitz <ari.abramowitz1@gmail.com>
2023-07-16 15:09:54 +0300

    fx: Initialize multiselects on dom change

--- static/js/base.js
+++ static/js/base.js
@@ -15,31 +15,46 @@
   );
 })();
 
-$(document).ready(function () {
-  $(".bs-multiselect").multiselect({
-    includeSelectAllOption: true,
-    buttonClass: "form-select",
-    buttonWidth: "100%",
-    buttonText: function (options, select) {
-      if (options.length === 0) {
-        return "--------";
-      } else if (options.length > 1) {
-        return `${options.length} selected`;
-      } else {
-        var labels = [];
-        options.each(function () {
-          if ($(this).attr("label") !== undefined) {
-            labels.push($(this).attr("label"));
-          } else {
-            labels.push($(this).html());
-          }
-        });
-        return labels.join(", ") + "";
-      }
-    },
-    templates: {
-      button:
-        '<button type="button" class="multiselect dropdown-toggle d-block" data-bs-toggle="dropdown"><div class="multiselect-selected-text text-start"></div></button>',
-    },
+(function initializeMultiselects() {
+  const initialize = () => {
+    $(".bs-multiselect").multiselect({
+      includeSelectAllOption: true,
+      buttonClass: "form-select",
+      buttonWidth: "100%",
+      buttonText: function (options, select) {
+        if (options.length === 0) {
+          return "--------";
+        } else if (options.length > 1) {
+          return `${options.length} selected`;
+        } else {
+          var labels = [];
+          options.each(function () {
+            if ($(this).attr("label") !== undefined) {
+              labels.push($(this).attr("label"));
+            } else {
+              labels.push($(this).html());
+            }
+          });
+          return labels.join(", ") + "";
+        }
+      },
+      templates: {
+        button:
+          '<button type="button" class="multiselect dropdown-toggle d-block" data-bs-toggle="dropdown"><div class="multiselect-selected-text text-start"></div></button>',
+      },
+    });
+  };
+
+  $(document).ready(function () {
+    initialize();
   });
-});
+  const observer = new MutationObserver((mutationsList, observer) => {
+    initialize();
+  });
+  const targetElement = document.querySelector("body");
+  const options = {
+    childList: true,
+    subtree: true,
+  };
+  observer.observe(targetElement, options);
+})();
