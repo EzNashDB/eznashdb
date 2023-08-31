dd6fffb3c974033b1193b21e994dd3748006a17a
Ari Abramowitz <ari.abramowitz1@gmail.com>
2023-08-31 21:43:31 +0300

    feat: Add initial city field (model + form)

--- eznashdb/models.py
+++ eznashdb/models.py
@@ -15,6 +15,7 @@ class Shul(models.Model):
     updated_by = ArrayField(models.IntegerField(), blank=True, default=list)
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=255, blank=True)
+    city = models.CharField(max_length=255, blank=True)
     has_female_leadership = models.BooleanField(null=True, blank=True)
     has_childcare = models.BooleanField(null=True, blank=True)
     can_say_kaddish = models.BooleanField(null=True, blank=True)
