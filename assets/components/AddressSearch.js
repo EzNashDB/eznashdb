08bc07711b788e35c8f5443934e1e15f32dc7912
Ari Abramowitz <ari.abramowitz1@gmail.com>
2023-10-05 17:40:09 +0300

    feat: Save lat, lon, place_id on shul form

--- assets/components/AddressSearch.js
+++ assets/components/AddressSearch.js
@@ -12,6 +12,7 @@ const SEARCH_URI = "/address-lookup";
 export const AddressSearch = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [options, setOptions] = useState([]);
+  const [selected, setSelected] = useState(null);
 
   const handleSearch = (query) => {
     setIsLoading(true);
@@ -26,56 +27,81 @@ export const AddressSearch = () => {
       });
   };
 
+  const handleChange = (options) => {
+    setSelected(options[0]);
+  };
+
   // Bypass client-side filtering by returning `true`. Results are already
   // filtered by the search endpoint, so no need to do it again.
   const filterBy = () => true;
   return (
-    <AsyncTypeahead
-      className="w-100 position-relative"
-      filterBy={filterBy}
-      id="address-select"
-      isLoading={isLoading}
-      labelKey="display_name"
-      minLength={3}
-      onSearch={handleSearch}
-      useCache={false}
-      options={options}
-      placeholder="Search for an address..."
-      inputProps={{
-        name: "address",
-        className: "textinput form-control",
-        autoComplete: "one-time-code",
-      }}
-      renderInput={({ inputRef, referenceElementRef, ...inputProps }) => (
-        <Hint>
-          <div className="input-group input-group-sm">
-            <Form.Control
-              {...inputProps}
-              ref={(node) => {
-                inputRef(node);
-                referenceElementRef(node);
-              }}
-            />
-          </div>
-        </Hint>
-      )}
-      renderMenu={(results, menuProps) => {
-        const {
-          newSelectionPrefix,
-          paginationText,
-          renderMenuItemChildren,
-          ..._menuProps
-        } = menuProps;
-        return (
-          <Menu {..._menuProps} className="position-fixed shadow-lg">
-            {results.map((result, index) => (
-              <MenuItem option={result} position={index} key={index}>
-                <span className="text-wrap">{result.display_name}</span>
-              </MenuItem>
-            ))}
-          </Menu>
-        );
-      }}
-    />
+    <>
+      <AsyncTypeahead
+        className="w-100 position-relative"
+        filterBy={filterBy}
+        id="address-select"
+        isLoading={isLoading}
+        labelKey="display_name"
+        minLength={3}
+        onSearch={handleSearch}
+        onChange={handleChange}
+        useCache={false}
+        options={options}
+        placeholder="Search for an address..."
+        inputProps={{
+          name: "address",
+          className: "textinput form-control",
+          autoComplete: "one-time-code",
+        }}
+        renderInput={({ inputRef, referenceElementRef, ...inputProps }) => (
+          <Hint>
+            <div className="input-group input-group-sm">
+              <Form.Control
+                {...inputProps}
+                ref={(node) => {
+                  inputRef(node);
+                  referenceElementRef(node);
+                }}
+              />
+            </div>
+          </Hint>
+        )}
+        renderMenu={(results, menuProps) => {
+          const {
+            newSelectionPrefix,
+            paginationText,
+            renderMenuItemChildren,
+            ..._menuProps
+          } = menuProps;
+          return (
+            <Menu {..._menuProps} className="position-fixed shadow-lg">
+              {results.map((result, index) => (
+                <MenuItem option={result} position={index} key={index}>
+                  <span className="text-wrap">{result.display_name}</span>
+                </MenuItem>
+              ))}
+            </Menu>
+          );
+        }}
+      />
+      <input
+        type="hidden"
+        name="latitude"
+        id="id_latitude"
+        value={selected?.lat || ""}
+      ></input>
+      <input
+        type="hidden"
+        name="longitude"
+        id="id_longitude"
+        value={selected?.lon || ""}
+      ></input>
+      <input
+        type="hidden"
+        name="place_id"
+        id="id_place_id"
+        value={selected?.place_id || ""}
+      ></input>
+    </>
   );
 };
