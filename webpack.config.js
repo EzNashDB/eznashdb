b0e046055d0826a200fbf1f85ddfc0157766f66b
Ari Abramowitz <ari.abramowitz1@gmail.com>
2023-10-28 22:13:15 +0300

    feat: Add pin clustering on map

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,22 @@ module.exports = {
         loader: "babel-loader",
         options: { presets: ["@babel/preset-env", "@babel/preset-react"] },
       },
+      {
+        test: /\.(sass|less|css)$/,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images/",
+            },
+          },
+        ],
+      },
     ],
   },
   mode: "development",
