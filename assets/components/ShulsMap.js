a7e76bb9625fa3196a9ce8e11223cec36fdf9778
Ari Abramowitz <ari.abramowitz1@gmail.com>
2023-11-01 20:02:24 +0200

    feat: Update url params on drag, zoom

--- assets/components/ShulsMap.js
+++ assets/components/ShulsMap.js
@@ -1,10 +1,30 @@
 import React, { useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
 import { ShulPopup } from "./ShulPopup";
 import "../css/map.css";
 
 export const ShulsMap = ({ shuls }) => {
+  const updateURLParam = (key, value) => {
+    const url = new URL(window.location.href);
+    url.searchParams.set(key, value);
+    const newURL = url.toString();
+    history.pushState({}, document.title, newURL);
+  };
+  const MapEvents = () => {
+    useMapEvents({
+      dragend: (e) => {
+        const center = e.target.getCenter();
+        updateURLParam("lat", center.lat);
+        updateURLParam("lon", center.lng);
+      },
+      zoomend: (e) => {
+        updateURLParam("zoom", e.target.getZoom());
+      },
+    });
+    return null;
+  };
+
   const latLonShuls = shuls.filter((shul) => shul.latitude && shul.longitude);
   const urlParams = new URLSearchParams(window.location.search);
   let startLat = urlParams.get("lat") || 20;
@@ -18,6 +38,7 @@ export const ShulsMap = ({ shuls }) => {
       scrollWheelZoom={true}
       style={{ height: "100%" }}
     >
+      <MapEvents />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
