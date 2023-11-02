e01dafa49cf0e00a0cbfca534fde9ec86d9d5099
Ari Abramowitz <ari.abramowitz1@gmail.com>
2023-11-02 10:42:14 +0200

    feat: Open selected popup on load

--- assets/components/ShulsMap.js
+++ assets/components/ShulsMap.js
@@ -1,25 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
 import { ShulPopup } from "./ShulPopup";
 import "../css/map.css";
 
 export const ShulsMap = ({ shuls }) => {
+  // Based on https://stackoverflow.com/a/67825424
+  const [markersDoneLoading, setMarkersDoneLoading] = React.useState(false);
+  const markerRefs = React.useRef({});
+
+  useEffect(() => {
+    let params = new URLSearchParams(document.location.search.substring(1));
+    let id = params.get("selectedPin");
+    if (id && markersDoneLoading) {
+      const markerToOpen = markerRefs.current[id];
+      if (markerToOpen) markerToOpen.openPopup();
+    }
+  }, [markersDoneLoading]);
+
   const copiedShuls = shuls
     .map((shul) => {
       const copy1 = { ...shul };
       const copy2 = { ...shul };
       copy1.longitude = (parseFloat(shul.longitude) + 360).toString();
       copy2.longitude = (parseFloat(shul.longitude) - 360).toString();
+      copy1.id = `+${shul.id}`;
+      copy2.id = `-${shul.id}`;
       return [shul, copy1, copy2];
     })
     .flat();
 
   const updateURLParams = (params) => {
     const url = new URL(window.location.href);
-    for (const key in params) {
-      url.searchParams.set(key, params[key]);
-    }
+    for (const key in params) url.searchParams.set(key, params[key]);
+    const newURL = url.toString();
+    history.replaceState({}, "", newURL);
+  };
+  const deleteURLParams = (params) => {
+    const url = new URL(window.location.href);
+    params.forEach((param) => url.searchParams.delete(param));
     const newURL = url.toString();
     history.replaceState({}, "", newURL);
   };
@@ -35,6 +54,8 @@ export const ShulsMap = ({ shuls }) => {
     useMapEvents({
       moveend: (e) => updateURLLocationParams(e),
       zoomend: (e) => updateURLLocationParams(e),
+      popupopen: (e) => updateURLParams({ selectedPin: e.popup.options.id }),
+      popupclose: (e) => deleteURLParams(["selectedPin"]),
     });
     return null;
   };
@@ -63,7 +84,16 @@ export const ShulsMap = ({ shuls }) => {
       />
       <MarkerClusterGroup chunkedLoading showCoverageOnHover={false}>
         {latLonShuls.map((shul, index) => (
-          <Marker key={index} position={[shul.latitude, shul.longitude]}>
+          <Marker
+            key={index}
+            position={[shul.latitude, shul.longitude]}
+            ref={(m) => {
+              markerRefs.current[shul.id] = m;
+              if (index === shuls.length - 1 && !markersDoneLoading) {
+                setMarkersDoneLoading(true);
+              }
+            }}
+          >
             <ShulPopup shul={shul} />
           </Marker>
         ))}
