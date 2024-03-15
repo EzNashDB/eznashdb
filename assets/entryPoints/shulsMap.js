afd6a851629de43361a9d54594131170e7cd8458
Ari Abramowitz <ari.abramowitz1@gmail.com>
2024-03-15 06:42:41 -0400

    feat: Add loader spinners on shuls map, shul count

--- assets/entryPoints/shulsMap.js
+++ assets/entryPoints/shulsMap.js
@@ -11,12 +11,39 @@ const renderShulCount = () => {
   const shulCountContainer = document.getElementById("shuls-count");
   const count = shuls.length;
   const shulsLabel = shuls.length == 1 ? "shul" : "shuls";
-  shulCountContainer.innerHTML = `${count} ${shulsLabel} found`;
+  shulCountContainer.innerHTML = `${count} ${shulsLabel}`;
 };
+
+class SpinnerManager {
+  constructor() {
+    this._spinnerOverlays = Array.from(
+      document.getElementsByClassName("spinner-overlay")
+    );
+  }
+
+  show() {
+    this._spinnerOverlays.forEach((spinner) => {
+      spinner.classList.remove("opacity-0");
+      spinner.classList.add("opacity-50");
+    });
+  }
+
+  hide() {
+    this._spinnerOverlays.forEach((spinner) => {
+      spinner.classList.add("opacity-0");
+      spinner.classList.remove("opacity-50");
+    });
+  }
+}
 
 ["DOMContentLoaded", "shulsDataLoaded"].forEach((eventType) => {
   document.addEventListener(eventType, (e) => {
+    new SpinnerManager().hide();
     renderMap();
     renderShulCount();
   });
+});
+
+document.addEventListener("htmx:beforeSend", (e) => {
+  new SpinnerManager().show();
 });
