fad613ac67487afac98a07fd0674a50b46753219
Ari Abramowitz <ari.abramowitz1@gmail.com>
2024-03-15 05:04:48 -0400

    feat: Update shuls count on shulsDataLoaded event

--- assets/entryPoints/shulsMap.js
+++ assets/entryPoints/shulsMap.js
@@ -2,9 +2,21 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { ShulsMap } from "../components/ShulsMap";
 
+const renderMap = () => {
+  const shulsMapContainer = document.getElementById("shulsMap");
+  createRoot(shulsMapContainer).render(<ShulsMap shuls={shuls} />);
+};
+
+const renderShulCount = () => {
+  const shulCountContainer = document.getElementById("shuls-count");
+  const count = shuls.length;
+  const shulsLabel = shuls.length == 1 ? "shul" : "shuls";
+  shulCountContainer.innerHTML = `${count} ${shulsLabel} found`;
+};
+
 ["DOMContentLoaded", "shulsDataLoaded"].forEach((eventType) => {
   document.addEventListener(eventType, (e) => {
-    const shulsMapContainer = document.getElementById("shulsMap");
-    createRoot(shulsMapContainer).render(<ShulsMap shuls={shuls} />);
+    renderMap();
+    renderShulCount();
   });
 });
