30e9b26abd585ab8d653165910dfb9ae939e4286
Ari Abramowitz <ari.abramowitz1@gmail.com>
2024-03-31 13:39:44 -0400

    fix: Only toggle scrollbox when needed; reassign focus on toggle

--- assets/initializers/shadowScroll.js
+++ assets/initializers/shadowScroll.js
@@ -1,5 +1,6 @@
 class ShadowScrollInitializer {
   constructor(wrapperEl) {
+    this.isScrollable = false;
     this.parentEl = wrapperEl;
     this.scrollBox = this.parentEl.querySelector(".shadow-scroll__scroll-box");
     this.content = this.parentEl.querySelector(".shadow-scroll__content");
@@ -21,7 +22,11 @@ class ShadowScrollInitializer {
   setVisibility() {
     const isOverflowing =
       this.content.offsetHeight > this.parentEl.offsetHeight;
-    isOverflowing ? this.show() : this.hide();
+    if (this.isScrollable !== isOverflowing) {
+      const activeElement = document.activeElement;
+      isOverflowing ? this.show() : this.hide();
+      activeElement.focus();
+    }
   }
 
   setShadows() {
@@ -42,12 +47,14 @@ class ShadowScrollInitializer {
     this.shadowWrapper.style.display = "block";
     this.shadowWrapper.style.borderWidth = "var(--bs-border-width)";
     this.scrollBox.appendChild(this.content);
+    this.isScrollable = true;
   }
 
   hide() {
     this.shadowWrapper.style.display = "none";
     this.shadowWrapper.style.borderWidth = "0";
     this.parentEl.appendChild(this.content);
+    this.isScrollable = false;
   }
 
   listenForScroll() {
