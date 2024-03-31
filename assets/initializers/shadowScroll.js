255eacafabf67b9136d86da2ceddc9ffd9f3b81a
Ari Abramowitz <ari.abramowitz1@gmail.com>
2024-03-31 14:40:03 -0400

    feat Make scroll shadow opacity constant
    Why
    Previously listened for scroll event to control opacity, which led to
    jumpy scrolling. Then tried throttling to avoid the jumpiness, but then
    the animation what slightly out of sync with the scroll. Now ditching the
    animation for a simpler solution

--- assets/initializers/shadowScroll.js
+++ assets/initializers/shadowScroll.js
@@ -1,5 +1,3 @@
-const throttle = require("lodash.throttle");
-
 class ShadowScrollInitializer {
   constructor(wrapperEl) {
     this.isScrollable = false;
@@ -9,15 +7,10 @@ class ShadowScrollInitializer {
     this.shadowWrapper = this.parentEl.querySelector(
       ".shadow-scroll__shadow-wrapper"
     );
-    this.shadowTop = this.parentEl.querySelector(".shadow-scroll__top-shadow");
-    this.shadowBottom = this.parentEl.querySelector(
-      ".shadow-scroll__bottom-shadow"
-    );
   }
 
   initialize() {
     this.setVisibility();
-    this.listenForScroll();
     this.listenForResize();
   }
 
@@ -31,21 +24,7 @@ class ShadowScrollInitializer {
     }
   }
 
-  setShadows() {
-    const scrollPercentage = this.calculateScrollPercentage(this.scrollBox);
-    this.shadowTop.style.opacity = scrollPercentage;
-    this.shadowBottom.style.opacity = 1 - scrollPercentage;
-  }
-
-  calculateScrollPercentage(element) {
-    const scrollableHeight = element.scrollHeight - element.clientHeight;
-    if (scrollableHeight === 0) return 0;
-    const scrollPercentage = (element.scrollTop / scrollableHeight) * 100;
-    return scrollPercentage / 100;
-  }
-
   show() {
-    this.setShadows();
     this.shadowWrapper.style.display = "block";
     this.shadowWrapper.style.borderWidth = "var(--bs-border-width)";
     this.scrollBox.appendChild(this.content);
@@ -57,13 +36,6 @@ class ShadowScrollInitializer {
     this.shadowWrapper.style.borderWidth = "0";
     this.parentEl.appendChild(this.content);
     this.isScrollable = false;
-  }
-
-  listenForScroll() {
-    this.scrollBox.addEventListener(
-      "scroll",
-      throttle((e) => this.setShadows(), 100)
-    );
   }
 
   listenForResize() {
