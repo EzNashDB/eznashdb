3cabe9413278feb38a9d633254dd65ad4627bac3
Ari Abramowitz <ari.abramowitz1@gmail.com>
2024-04-07 10:23:05 -0400

    feat: Add childcare to shul serializer

--- eznashdb/serializers.py
+++ eznashdb/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
 
-from eznashdb.models import Room, Shul, ShulLink
+from eznashdb.models import ChildcareProgram, Room, Shul, ShulLink
 
 
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
+        exclude = ["shul"]
+
+
+class ChildcareSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChildcareProgram
         exclude = ["shul"]
 
 
@@ -17,6 +23,7 @@ class LinkSerializer(serializers.ModelSerializer):
 
 class ShulSerializer(serializers.ModelSerializer):
     rooms = serializers.SerializerMethodField()
+    childcare_programs = serializers.SerializerMethodField()
     links = LinkSerializer(many=True)
 
     class Meta:
@@ -26,3 +33,7 @@ class ShulSerializer(serializers.ModelSerializer):
     def get_rooms(self, obj):
         ordered_rooms = obj.rooms.order_by("pk")
         return RoomSerializer(ordered_rooms, many=True).data
+
+    def get_childcare_programs(self, obj):
+        ordered_programs = obj.childcare_programs.order_by("pk")
+        return ChildcareSerializer(ordered_programs, many=True).data
