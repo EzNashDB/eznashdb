0b4b31707cbdd412593d1992d5ff5b2e5ffa15cf
Ari Abramowitz <ari.abramowitz1@gmail.com>
2025-11-10 16:14:33 -0500

    fix: Allow escaped html in dropdown options in firefox

--- eznashdb/filters.py
+++ eznashdb/filters.py
@@ -2,7 +2,7 @@ from django.db.models import Q
 from django_filters import MultipleChoiceFilter
 
 from eznashdb.constants import DEFAULT_ARG
-from eznashdb.widgets import MultiSelectWidget
+from eznashdb.widgets import MultiTomSelectWidget
 
 
 class UnknownChoiceMixin:
@@ -17,7 +17,7 @@ class MultiSelectModelFieldFilter(MultipleChoiceFilter):
     def __init__(
         self, label, model_field, choices: list[tuple[str, str]] = DEFAULT_ARG, *args, **kwargs
     ):
-        widget = kwargs.pop("widget", MultiSelectWidget)
+        widget = kwargs.pop("widget", MultiTomSelectWidget)
         super().__init__(*args, choices=tuple(choices), label=label, widget=widget, **kwargs)
         self.model_field = model_field
         self.method = kwargs.pop("method", self.filter_method)
