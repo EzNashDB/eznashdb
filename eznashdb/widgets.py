b49b2e3d4c7b323ff3a5998922b2cccfe1c5f07d
Ari Abramowitz <ari.abramowitz1@gmail.com>
2025-11-19 14:17:47 -0500

    feat: Remove small classes on forms, filters

--- eznashdb/widgets.py
+++ eznashdb/widgets.py
@@ -32,7 +32,8 @@ class DataHtmlOptionMixin:
 class TomSelectMixin:
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
-        context["widget"]["attrs"]["class"] += " tom-select"
+        attrs = context["widget"]["attrs"]
+        attrs["class"] = attrs.get("class", "") + " tom-select"
         return context
 
 
