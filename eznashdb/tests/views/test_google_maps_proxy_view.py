365eb8fd3a1c8d7288e08ce253aee06a5a716784
Ari Abramowitz <ari.abramowitz1@gmail.com>
2025-11-26 15:38:43 -0500

    feat: Use coords-only in google maps url

--- eznashdb/tests/views/test_google_maps_proxy_view.py
+++ eznashdb/tests/views/test_google_maps_proxy_view.py
@@ -1,12 +1,11 @@
-import urllib.parse
-
 import pytest
 from django.urls import reverse
 
 
 @pytest.mark.django_db
-def test_google_maps_redirect_uses_address_field_with_full_street_address(client, test_shul):
-    test_shul.address = "Synagogue Ohel Abraham, Rue de Montévidéo, Paris 75116"
+def test_google_maps_redirect_uses_coordinates(client, test_shul):
+    test_shul.latitude = 48.86837
+    test_shul.longitude = 2.29348
     test_shul.save()
 
     url = reverse("eznashdb:google_maps_proxy")
@@ -14,19 +13,21 @@ def test_google_maps_redirect_uses_address_field_with_full_street_address(client
 
     assert response.status_code == 302
     assert response.url.startswith("https://www.google.com/maps")
-    assert urllib.parse.quote_plus(test_shul.address) in response.url
+    assert "48.86837,2.29348" in response.url
 
 
 @pytest.mark.django_db
-def test_google_maps_redirect_uses_address_field_with_lat_lon_coords(client, test_shul):
-    test_shul.address = "40.913415282803335,-74.01149690151216"
+def test_google_maps_redirect_rounds_coordinates(client, test_shul):
+    test_shul.latitude = 40.913415282803335
+    test_shul.longitude = -74.01149690151216
     test_shul.save()
 
     url = reverse("eznashdb:google_maps_proxy")
     response = client.get(url, data={"id": test_shul.id})
+
     assert response.status_code == 302
-    assert response.url.startswith("https://www.google.com/maps")
-    assert urllib.parse.quote_plus(test_shul.address) in response.url
+    # Check that coordinates are rounded to 5 decimal places
+    assert "40.91342,-74.0115" in response.url
 
 
 @pytest.mark.django_db
@@ -41,15 +42,3 @@ def test_google_maps_redirect_missing_id(client):
     url = reverse("eznashdb:google_maps_proxy")
     response = client.get(url)
     assert response.status_code == 400
-
-
-@pytest.mark.django_db
-def test_google_maps_redirect_unicode_address(client, test_shul):
-    test_shul.address = "בית הכנסת הגדול, 56, המלך ג׳ורג׳, מחנה ישראל, ירושלים | القدس, נפת ירושלים, מחוז ירושלים, 9426222, ישראל"
-    test_shul.save()
-
-    url = reverse("eznashdb:google_maps_proxy")
-    response = client.get(url, data={"id": test_shul.id})
-
-    assert response.status_code == 302
-    assert urllib.parse.quote_plus(test_shul.address) in response.url
