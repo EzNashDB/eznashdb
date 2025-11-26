7ee14d507f92666cd54969f6ce93efebf3ef5802
Ari Abramowitz <ari.abramowitz1@gmail.com>
2025-11-26 04:37:30 -0500

    feat: Add shul details to admin page

--- eznashdb/admin.py
+++ eznashdb/admin.py
@@ -1,8 +1,29 @@
 from django.apps import apps
 from django.contrib import admin
 
+from .models import Shul  # explicitly import Shul for customization
+
+
+# Custom admin for Shul
+class ShulAdmin(admin.ModelAdmin):
+    list_display = ("name", "address", "created_at", "updated_at")
+    readonly_fields = ("created_at", "updated_at")
+    fieldsets = (
+        (None, {"fields": ("name", "address")}),
+        (
+            "Timestamps",
+            {
+                "fields": ("created_at", "updated_at"),
+            },
+        ),
+    )
+
+
+# Register Shul with custom admin
+admin.site.register(Shul, ShulAdmin)
+
+# Dynamically register all other models
 eznashdb_models = apps.get_app_config("eznashdb").models
-
-
 for _model_name, model in eznashdb_models.items():
-    admin.site.register(model)
+    if model is not Shul:  # skip Shul since we already registered it
+        admin.site.register(model)
