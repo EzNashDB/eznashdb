158220e0b1bcf9481477e0660cd93664a6af719c
Ari Abramowitz <ari.abramowitz1@gmail.com>
2025-12-04 04:41:22 -0500

    feat: Add wysiwyg editor for about page

--- eznashdb/admin.py
+++ eznashdb/admin.py
@@ -1,7 +1,11 @@
+from django import forms
 from django.apps import apps
 from django.contrib import admin
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+from tinymce.widgets import TinyMCE
 
-from .models import Shul  # explicitly import Shul for customization
+from eznashdb.models import Shul
 
 
 # Custom admin for Shul — only affects list page
@@ -17,3 +21,19 @@ eznashdb_models = apps.get_app_config("eznashdb").models
 for _model_name, model in eznashdb_models.items():
     if model is not Shul:  # skip Shul since we already registered it
         admin.site.register(model)
+
+
+class FlatPageForm(forms.ModelForm):
+    content = forms.CharField(widget=TinyMCE())
+
+    class Meta:
+        model = FlatPage
+        fields = ["url", "title", "content", "sites", "registration_required", "template_name"]
+
+
+admin.site.unregister(FlatPage)
+
+
+@admin.register(FlatPage)
+class CustomFlatPageAdmin(FlatPageAdmin):
+    form = FlatPageForm
