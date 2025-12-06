1e4d2acfb456b62107931148fa659c55b246dfe2
Ari Abramowitz <ari.abramowitz1@gmail.com>
2025-12-06 15:28:05 -0500

    feat: Set sentry sample rates to 0 to stop memory leak

--- app/settings.py
+++ app/settings.py
@@ -258,13 +258,8 @@ CRISPY_CLASS_CONVERTERS = {"textinput": "textinput rounded", "select": "select r
 if SENTRY_DSN := os.environ.get("SENTRY_DSN"):
     sentry_sdk.init(
         dsn=SENTRY_DSN,
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        traces_sample_rate=1.0,
-        # Set profiles_sample_rate to 1.0 to profile 100%
-        # of sampled transactions.
-        # We recommend adjusting this value in production.
-        profiles_sample_rate=1.0,
+        traces_sample_rate=0,
+        profiles_sample_rate=0,
     )
 
 # Geocoding
