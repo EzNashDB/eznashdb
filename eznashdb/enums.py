cc1397627654f326509f90729df21f58c3d9bce9
Ari Abramowitz <ari.abramowitz1@gmail.com>
2025-12-09 15:07:08 -0500

    refactor: Refactor enums.py

--- eznashdb/enums.py
+++ eznashdb/enums.py
@@ -2,24 +2,24 @@ from django.db import models
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 
-BLANK_CHOICE = ("", "-------")
+
+class DisplayChoicesMixin:
+    """Mixin to provide custom display choices for TextChoices enums."""
+
+    def get_display(self):
+        """Override in subclass to provide custom display HTML."""
+        raise NotImplementedError
+
+    @classmethod
+    def get_display_choices(cls, include_blank=False):
+        """Generate choices with custom display HTML."""
+        choices = [(choice.value, choice.get_display()) for choice in cls]
+        if include_blank:
+            choices = [("", "-------")] + choices
+        return choices
 
 
-def get_size_option_display(letter, explanation):
-    return f"""
-        <div class='d-inline-flex gap-1'>
-            <div>
-                <kbd class='fw-bold'>{letter}</kbd>
-            </div>
-            -
-            <div class='flex-grow-1 overflow-hidden text-break'>
-                {explanation}
-            </div>
-        </div>
-    """
-
-
-class RelativeSize(models.TextChoices):
+class RelativeSize(DisplayChoicesMixin, models.TextChoices):
     L = "L", _("Equal or larger than men's")
     M = "M", _("At least half men's size")
     S = "S", _("Less than half men's size")
@@ -27,28 +27,18 @@ class RelativeSize(models.TextChoices):
     MIXED = "&", _("Mixed seating")
 
     def get_display(self):
-        return mark_safe(get_size_option_display(self.value, self.label))
-
-    @classmethod
-    def get_display_choices(cls, include_blank=False):
-        choices = [(c.value, c.get_display()) for c in cls]
-        if include_blank:
-            choices = [BLANK_CHOICE] + choices
-        return choices
-
-
-def get_star_display(score):
-    divs = []
-    for i in range(5):
-        if i < score:
-            divs.append("<i class='fa-solid fa-star text-warning'></i>")
-        else:
-            divs.append("<i class='fa-regular fa-star text-warning'></i>")
-    stars = "".join(divs)
-    return f"<span class='text-nowrap'><kbd class='fw-bold'>{score}</kbd> - {stars}</span>"
+        return mark_safe(
+            f"""
+            <div class='d-inline-flex gap-1'>
+                <div><kbd class='fw-bold'>{self.value}</kbd></div>
+                -
+                <div class='flex-grow-1 overflow-hidden text-break'>{self.label}</div>
+            </div>
+            """
+        )
 
 
-class SeeHearScore(models.TextChoices):
+class SeeHearScore(DisplayChoicesMixin, models.TextChoices):
     _5 = "5"
     _4 = "4"
     _3 = "3"
@@ -56,11 +46,15 @@ class SeeHearScore(models.TextChoices):
     _1 = "1"
 
     def get_display(self):
-        return mark_safe(get_star_display(int(self)))
+        score = int(self.value)
+        stars = self._render_stars(score)
+        return mark_safe(
+            f"<span class='text-nowrap'><kbd class='fw-bold'>{score}</kbd> - {stars}</span>"
+        )
 
-    @classmethod
-    def get_display_choices(cls, include_blank=False):
-        choices = [(c.value, c.get_display()) for c in cls]
-        if include_blank:
-            choices = [BLANK_CHOICE] + choices
-        return choices
+    @staticmethod
+    def _render_stars(score):
+        """Render star icons based on score."""
+        filled = "<i class='fa-solid fa-star text-warning'></i>"
+        empty = "<i class='fa-regular fa-star text-warning'></i>"
+        return "".join(filled if i < score else empty for i in range(5))
