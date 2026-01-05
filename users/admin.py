56e00e4a387c25e7d07a0a06c55d728a1ffaff33
Ari Abramowitz <ari.abramowitz1@gmail.com>
2026-01-05 15:58:13 -0500

    feat: Add user's full name to user admin

--- users/admin.py
+++ users/admin.py
@@ -8,7 +8,18 @@ from users.models import User
 class UserAdmin(BaseUserAdmin):
     """Admin interface for User model."""
 
-    list_display = ["email", "username", "is_staff", "is_active", "date_joined"]
+    list_display = [
+        "email",
+        "username",
+        "full_name",
+        "is_staff",
+        "is_active",
+        "date_joined",
+    ]
     list_filter = ["is_staff", "is_active", "date_joined"]
-    search_fields = ["email", "username"]
+    search_fields = ["email", "username", "first_name", "last_name"]
     ordering = ["-date_joined"]
+
+    @admin.display(description="Name")
+    def full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}".strip()
