d562d106afc1edd821b7cf4a40ff7f82523460f7
Ari Abramowitz <ari.abramowitz1@gmail.com>
2026-01-20 11:38:01 -0500

    feat: Wrap spinner code in iife, use single constructor

--- eznashdb/static/eznashdb/js/map_spinner.js
+++ eznashdb/static/eznashdb/js/map_spinner.js
@@ -1,41 +1,45 @@
-class SpinnerManager {
-  constructor() {
-    this._spinnerOverlays = Array.from(
-      document.getElementsByClassName("spinner-overlay")
-    );
+(() => {
+  class SpinnerManager {
+    constructor() {
+      this._spinnerOverlays = Array.from(
+        document.getElementsByClassName("spinner-overlay")
+      );
+    }
+
+    show() {
+      this._spinnerOverlays.forEach((spinner) => {
+        spinner.classList.remove("opacity-0");
+        spinner.classList.add("opacity-50");
+      });
+    }
+
+    hide() {
+      this._spinnerOverlays.forEach((spinner) => {
+        spinner.classList.add("opacity-0");
+        spinner.classList.remove("opacity-50");
+      });
+    }
   }
 
-  show() {
-    this._spinnerOverlays.forEach((spinner) => {
-      spinner.classList.remove("opacity-0");
-      spinner.classList.add("opacity-50");
-    });
-  }
+  const spinnerManager = new SpinnerManager();
 
-  hide() {
-    this._spinnerOverlays.forEach((spinner) => {
-      spinner.classList.add("opacity-0");
-      spinner.classList.remove("opacity-50");
-    });
-  }
-}
+  document.addEventListener("htmx:beforeSend", (e) => {
+    // Show spinner only for elements with data-show-spinner
+    const shouldShowSpinner = e.target.closest("[data-show-spinner]");
+    if (shouldShowSpinner) {
+      spinnerManager.show();
+    }
+  });
 
-document.addEventListener("htmx:beforeSend", (e) => {
-  // Show spinner only for elements with data-show-spinner
-  const shouldShowSpinner = e.target.closest("[data-show-spinner]");
-  if (shouldShowSpinner) {
-    new SpinnerManager().show();
-  }
-});
+  document.addEventListener("htmx:afterSwap", (e) => {
+    // Hide spinner after HTMX swaps (but shulsDataLoaded will also hide it)
+    const shouldShowSpinner = e.target.closest("[data-show-spinner]");
+    if (shouldShowSpinner) {
+      spinnerManager.hide();
+    }
+  });
 
-document.addEventListener("htmx:afterSwap", (e) => {
-  // Hide spinner after HTMX swaps (but shulsDataLoaded will also hide it)
-  const shouldShowSpinner = e.target.closest("[data-show-spinner]");
-  if (shouldShowSpinner) {
-    new SpinnerManager().hide();
-  }
-});
-
-document.addEventListener("shulsDataLoaded", (e) => {
-  new SpinnerManager().hide();
-});
+  document.addEventListener("shulsDataLoaded", () => {
+    spinnerManager.hide();
+  });
+})();
