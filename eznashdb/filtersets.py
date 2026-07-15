a26d114ac8545468c2178333bbaf6bebf8c468cc
Ari Abramowitz <ari.abramowitz1@gmail.com>
2026-07-15 05:15:02 -0400

    feat: Remove name filter when kaddish flag active

--- eznashdb/filtersets.py
+++ eznashdb/filtersets.py
@@ -31,14 +31,15 @@ class ShulFilterSet(FilterSet):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if not flag_is_active(self.request, "kaddish"):
+        if flag_is_active(self.request, "kaddish"):
+            del self.filters["name"]
+        else:
             del self.filters["kaddish_policy"]
-
-        names = Shul.objects.values_list("name", flat=True).distinct().order_by("name")
-        choices = [(name, name) for name in names]
-        self.filters["name"].extra["choices"] = choices
-        self.form.fields["name"].choices = choices
-        self.form.fields["name"].widget = SearchableMultiTomSelectWidget(choices=choices)
+            names = Shul.objects.values_list("name", flat=True).distinct().order_by("name")
+            choices = [(name, name) for name in names]
+            self.filters["name"].extra["choices"] = choices
+            self.form.fields["name"].choices = choices
+            self.form.fields["name"].widget = SearchableMultiTomSelectWidget(choices=choices)
 
     def filter_rooms__relative_size(self, qs, name, value):
         query = Q(rooms__relative_size__in=value)
